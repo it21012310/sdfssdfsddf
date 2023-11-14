a58b9c2b36babae1461e356cfba1b5c3c06cfe85
Ravindu Wijesundara <87409925+it21012310@users.noreply.github.com>
2023-11-14 11:49:12 +0530

    Update Question01.java

--- Question01.java
+++ Question01.java
@@ -247,6 +247,82 @@ public static void main(String[] args) {
 
 }
 
+//Thread02
+
+package hello;
+
+public class EvenOddPrinter {
+
+	private static final int MAX_NUM = 20;
+	private static volatile int nextNum = 1;
+	private static final Object lock = new Object();
+	
+	public static void main(String[] args) {
+		Thread oddThread = new Thread(new OddRunnable(), "OddThread");
+		Thread evenThread = new Thread(new EvenRunnable(), "EvenThread");
+		
+		oddThread.start();
+		evenThread.start();
+		
+		try {
+			oddThread.join();
+			evenThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	static class OddRunnable implements Runnable{
+		public void run() {
+			while(nextNum <= MAX_NUM) {
+				synchronized (lock) {
+					if(nextNum % 2 == 1) {
+						System.out.println(Thread.currentThread().getName() + ";" + nextNum);
+						nextNum++;
+						lock.notify();
+					}
+					else {
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	static class EvenRunnable implements Runnable{
+		public void run() {
+			while(nextNum <= MAX_NUM) {
+				synchronized (lock) {
+					if(nextNum % 2 == 0) {
+						System.out.println(Thread.currentThread().getName() + ";" + nextNum);
+						nextNum++;
+						lock.notify();
+					}
+					else {
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	
+}
 
 
 
